1ebe1ea166947ede3be5cd1ef0fdcf4b5302d752
yann-nintunze-vinci <yann.nintunze@student.vinci.be>
2025-10-16 18:24:13 +0200

    edit: lesson 4

--- exercices/lesson-4-advanced-state/backend/db-populate.js
+++ exercices/lesson-4-advanced-state/backend/db-populate.js
@@ -1,33 +1,60 @@
 const { PrismaClient } = require("./generated/prisma");
 
+const alice = await prisma.user.upsert({
+  where: { email: "alice@expenso.dev" },
+  update: {},
+  create: {
+    name: "Alice",
+    email: "alice@expenso.dev",
+  },
+});
+
+const bob = await prisma.user.upsert({
+  where: { email: "bob@expenso.devm" },
+  update: {},
+  create: {
+    name: "Bob",
+    email: "bob@expenso.dev",
+  },
+});
+
 const defaultExpenses = [
   {
-    date: "2025-01-16T10:20:00.000Z",
+    date: new Date("2025-01-16T10:20:00.000Z"),
     description: "Example expense #1 from Alice",
-    payer: "Alice",
+    payerId: alice.id,
     amount: 25.5,
   },
   {
-    date: "2025-01-15T19:10:00.000Z",
+    date: new Date("2025-01-15T19:10:00.000Z"),
     description: "Example expense #2 from Bob",
-    payer: "Bob",
+    payerId: bob.id,
     amount: 35,
   },
   {
-    date: "2025-01-15T14:40:00.000Z",
+    date: new Date("2025-01-15T14:40:00.000Z"),
     description: "Example expense #3 from Alice",
-    payer: "Alice",
+    payerId: alice.id,
     amount: 2,
   },
 ];
 
-const prisma = new PrismaClient();
+await prisma.expense.createMany({
+  data: defaultExpenses,
+});
 
-async function main() {
-  await prisma.expense.createMany({
-    data: defaultExpenses,
-  });
-}
+await prisma.transfert.createMany({
+  data: [
+    {
+      sourceId: alice.id,
+      targetId: bob.id,
+    },
+    {
+      sourceId: bob.id,
+      targetId: alice.id,
+    },
+  ],
+});
 
 main()
   .finally(async () => {
